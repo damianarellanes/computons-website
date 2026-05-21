ddb93186559b7bcbc892353a5327be4649e7ee6d
damianarellanes <damian.arellanes@gmail.com>
2026-05-21 19:58:35 +0100

    removed preload

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,12 @@
 			$body.addClass('is-ie');
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
+		/*$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
-		});
+		});*/
+		$body.removeClass('is-preload');
 
 	// Forms.
 
